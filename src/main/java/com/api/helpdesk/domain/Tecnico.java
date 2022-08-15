e5f91b366627f22956e70072c93ebf9d1dfe3f95
Jamerson Souza <jamersonsouza@outlook.com>
2022-08-14 21:29:55 -0300

    Classes de Dominio criadas

---
+++ src/main/java/com/api/helpdesk/domain/Tecnico.java
@@ -0,0 +1,28 @@
+package com.api.helpdesk.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tecnico extends Pessoa{
+    
+    private List<Chamado> chamados = new ArrayList<>();
+
+    public Tecnico() {
+    }
+
+    public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
+        super(id, nome, cpf, email, senha);
+    }
+
+    
+    public List<Chamado> getChamados() {
+        return chamados;
+    }
+
+    public void setChamados(List<Chamado> chamados) {
+        this.chamados = chamados;
+    }
+
+    
+
+}
